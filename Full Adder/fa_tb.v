ced7360d4d75e437a22d9137062751a73a33b230
Pavan VL <pavan.vl786@gmail.com>
2024-07-24 23:31:34 +0530

    Add files via upload

---
+++ Full Adder/fa_tb.v
@@ -0,0 +1,23 @@
+//Designed by Pavan V L	
+module fa_tb;
+reg a,b,cin;
+wire s,co;
+
+fa fa_inst(a,b,cin,s,co);
+
+initial begin
+a=0;
+b=0;
+cin=0;
+$monitor(" a=%0b ",a," b=%0b ",b," cin=%0b ",cin," sum=%0b ",s," co=%0b ",co);
+#80 $finish;
+end
+
+always 
+#10 a=~a;
+always
+#20 b=~b;
+always
+#40 cin=~cin;
+
+endmodule
