42d40852e506ac3585c3bc423ac71d2c632c4635
Pavan VL <pavan.vl786@gmail.com>
2024-07-26 22:17:50 +0530

    Update ripplecarry.v

--- Ripple Carry Adder/ripplecarry.v
+++ Ripple Carry Adder/ripplecarry.v
@@ -8,8 +8,8 @@ output co;
 wire c1,c2,c3;
 
 fa fa_inst1(a[0],b[0],cin,s[0],c1);
-fa fa_inst1(a[1],b[1],c1,s[1],c2);
-fa fa_inst1(a[2],b[1],c2,s[2],c3);
-fa fa_inst1(a[3],b[1],c3,s[3],co);
+fa fa_inst2(a[1],b[1],c1,s[1],c2);
+fa fa_inst3(a[2],b[1],c2,s[2],c3);
+fa fa_inst4(a[3],b[1],c3,s[3],co);
 
 endmodule
