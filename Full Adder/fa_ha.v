8d1f0b58670fc7392cde6a2d73508ef6ef34ac19
Pavan VL <pavan.vl786@gmail.com>
2024-07-27 21:53:44 +0530

    Update fa_ha.v

--- Full Adder/fa_ha.v
+++ Full Adder/fa_ha.v
@@ -1,4 +1,5 @@
 //Designed by Pavan V L
+//Full adder using 2 half adders
 module fa_ha(a,b,cin,s,co);
 input a,b,cin;
 output s,co;
@@ -7,6 +8,6 @@ wire ha1_sum, ha1_co, ha2_co, carry_total;
 
 ha ha_insta1(.a(a), .b(b), .s(ha1_sum), .co(h1_co));
 ha ha_insta2(.a(ha1_sum), .b(cin) , .s(s) , .co(h2_co));
-or or_insta(co,h1_co,h2_co);
+or or_inst(co,h1_co,h2_co);
 
 endmodule
