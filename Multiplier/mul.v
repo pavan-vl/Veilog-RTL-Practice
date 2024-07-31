161fe633ec28d8b74022ee0130e90f1b231be109
Pavan VL <pavan.vl786@gmail.com>
2024-08-01 02:56:58 +0530

    Update mul.v

--- Multiplier/mul.v
+++ Multiplier/mul.v
@@ -6,21 +6,21 @@ output reg signed [7:0]y;
 reg [1:0]tmp;
 integer i;
 reg curr_val;
-reg [3:0]neg_y;
+reg [3:0]neg_b;
 always @ (*) begin
 y=8'd0;
 curr_val =1'd0;
 for(i=0;i<4;i=i+1) begin
-	tmp={x[i],curr_val}; //Concatinate and check condition
-	neg_y=-y;
+	tmp={a[i],curr_val}; //Concatinate and check condition
+	neg_b=-b;
 	case(tmp)
-		2'b10: y[7:4]=y[7:4] + neg_y;
-		2'b01: y[7:4]=y[7:4] + y;
+		2'b10: y[7:4]=y[7:4] + neg_b;
+		2'b01: y[7:4]=y[7:4] + b;
 		default:;
 	endcase
 	y=y>>1; //ASR
-	y[7]=y[6]; //preserve sign bit
-	curr_val = x[i]; //Store current value 
+	y[7]=y[6]; //Preserve sign bit
+	curr_val = a[i]; //Store current value 
 end
 if(b == 4'd8)
 	y=-y;
