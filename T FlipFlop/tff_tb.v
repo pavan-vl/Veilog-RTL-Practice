81f4505cf42eb14238895d1768e47150968a52e3
Pavan VL <pavan.vl786@gmail.com>
2024-08-04 22:17:20 +0530

    Update tff_tb.v

--- T FlipFlop/tff_tb.v
+++ T FlipFlop/tff_tb.v
@@ -4,12 +4,12 @@ reg t,clk,rst;
 wire q,qb;
 
 tff_sync tff_sync_inst(t,clk,rst,q,qb);
-//dff_async dff_async_inst(d,clk,rst,q,qb);  //For asynchronous reset
+//tff_async tff_async_inst(t,clk,rst,q,qb);  //For asynchronous reset
 
 initial begin
 t=0;
 rst=1;
-// rst=0; //For Asynchronous
+//rst=0; //For Asynchronous
 clk=0;
 $monitor(" time=%0d ns  t = %0b  rst = %0b  q = %0b  qb = %0b  ",$time,t,rst,q,qb);
 #90 $finish;
@@ -21,10 +21,7 @@ always #30 t=~t;
 
 initial #10 rst=0;
 
-/* For Asynchronous
-initial #10 rst=1;
-
-initial #35 rst=0;*/
-
+/*For Asynchronous
+initial #10 rst=1;*/
 
 endmodule
