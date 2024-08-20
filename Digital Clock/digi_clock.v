b9d140dd54819230ca546df43d4631c96f08d301
Pavan VL <pavan.vl786@gmail.com>
2024-08-21 01:04:16 +0530

    Update digi_clock.v

--- Digital Clock/digi_clock.v
+++ Digital Clock/digi_clock.v
@@ -1,58 +1,44 @@
 //Designed Pavan V L
-module digi_clock(clk,rst,sec,min,hr);
-input wire clk // Taking 10Hz Clock signal
-,rst;      
+module digi_clock(clk,sec,min,hr);
+input clk;      
 output reg [5:0] sec, min; 
 output reg [4:0] hr; 
 
-reg [3:0] clk_div; 
+
+reg [6:0] cnt; 
+
+initial begin
+sec=6'd1;
+min=6'd0;
+hr=5'd0;
+cnt=7'd0;
+end
+
 
 always @(posedge clk) begin
-if (rst)
-clk_div <= 0;
+if(cnt==7'd99) begin
+cnt<=7'd0;
+end
 else begin
-if (clk_div==4'd9) begin 
-clk_div <= 0;
-end 
-else begin
-clk_div <= clk_div + 1;
+cnt<=cnt+1;
 end
 end
-end
-
-wire clk_1s = (clk_div == 4'd9);
 
 always @(posedge clk) begin
-if (rst)
-sec <= 0;
-else if (clk_1s) begin
-if (sec == 6'd59) 
-sec <= 0;
-else 
+if (sec==6'd60 && cnt==7'd99) begin
+	sec<=6'd0;
+	min<=min+1;
+	if(min==6'd59) begin
+		min<=6'd0;
+		hr<=hr+1;
+		if(hr==5'd23) begin
+		hr<=5'd0;
+		end
+	end
+end
+
+else if(sec!=6'd60 && cnt==7'd99) begin
 sec<=sec+1;
 end
 end
-
-always @(posedge clk) begin
-if (rst)
-min<=0;
-else if (clk_1s && (sec==6'd59)) begin
-    if (min==6'd59)
-		min<=0;
-    else
-        min <= min + 1;
-    end
-end
-
-always @(posedge clk or posedge rst) begin
-if (rst)
-hr<=0;
-else if (clk_1s && (sec==6'd59) && (min==6'd59)) begin
-if (hr==5'd23)
-hr <= 0;
-else 
-hr <= hr + 1;
-end
-end
-
 endmodule
