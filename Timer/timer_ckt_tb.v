881527101fd8a268be8e35484f8cf832da796e3e
Pavan VL <pavan.vl786@gmail.com>
2024-08-24 00:46:43 +0530

    Update timer_ckt_tb.v

--- Timer/timer_ckt_tb.v
+++ Timer/timer_ckt_tb.v
@@ -1,14 +1,26 @@
 //Designed by Pavan V L
 module timer_ckt_tb;
-reg clk,rst;
+reg clk,stop_sig;
 reg [3:0]time_dur;
 wire out_int;
+wire [3:0]cnt;
 
-timer_ckt timer_ckt_inst(time_dur,out_int,rst,clk);
+timer_ckt timer_ckt_inst(time_dur,out_int,clk,cnt,stop_sig);
 
 initial begin
 clk=0;
-rst=1;
-$monitor(" Time Duration =%d  Reset=%b  Output=%b ",time_dur,rst,out);
-#200 $finish;
+stop_sig=0;
+time_dur=4'd3;
+$monitor(" Time Duration= %d  Current Timer Value= %d  Output= %b Stop?(%s)",time_dur,cnt,out_int,((stop_sig)?"Yes":"No"));
+#250 $finish;
 end
+
+always #5 clk= ~clk;
+
+
+
+initial #50 time_dur=4'd5;
+
+initial #175 stop_sig=1;
+
+endmodule
