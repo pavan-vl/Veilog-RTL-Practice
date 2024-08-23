4efc18bb61afe24f0f6fd4558fa7d5ccff9a5a38
Pavan VL <pavan.vl786@gmail.com>
2024-08-24 05:12:10 +0530

    Update pwm.v

--- PWM Generator/pwm.v
+++ PWM Generator/pwm.v
@@ -2,25 +2,19 @@
 module pwm(duty,clk,rst,pwm);
 input clk,rst;
 input [3:0]duty;
-output reg pwm;
+output pwm;
 reg [3:0]cnt;
 
 always @ (posedge clk) begin
 	if(rst) begin
-	pwm=4'd0;
 	cnt=4'd0;
 end
 else begin
-	if(cnt==4'd12)
+	if(cnt==4'd15)
 	cnt<=4'd0;
 	else
 	cnt<=cnt+1;
-	
-	if(cnt<duty) begin
-		pwm=1; 
-	end
-	else
-		pwm=0;
 end
-
+end
+assign pwm=(cnt<duty)?1:0;
 endmodule
