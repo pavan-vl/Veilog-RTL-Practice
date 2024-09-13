fa531221f4e3a2ecab3d7c11150c6c51f06b1760
Pavan VL <pavan.vl786@gmail.com>
2024-09-14 00:17:05 +0530

    Update watch_dog_timer.v

--- Watch Dog Timer/watch_dog_timer.v
+++ Watch Dog Timer/watch_dog_timer.v
@@ -3,26 +3,28 @@ module watch_dog_timer(clk,rst,wd_en,rst_o);
 input clk,rst,wd_en;
 output reg rst_o;
 
-parameter wait_time= 500; //Nuber of clock cycles to wait to send reset signal
+//parameter wait_time= 100; //Nuber of clock cycles to wait to send reset signal
 
-reg [8:0] cnt; //9bits to store 500
+reg [6:0] cnt; //9bits to store 100
 
 always @ (posedge clk or posedge rst) begin
 if(rst) begin
 rst_o<=0;
 cnt<=0;
 end
-else begin
+else begin 
 	if(wd_en) begin
-		cnt<=0;
-		rst_o<=0;
+		if(cnt<7'd99) begin
+		    cnt<= cnt+ 7'b1;
+		end
+		else begin
+			rst_o<=1;
+			cnt<=0;
+		end
 	end
 	else begin
-		if(cnt==wait_time)
-			rst_o<=1;
-		else begin
-			cn<= cnt+ 1;
-		end
+	   cnt<=0;
+	   rst_o=0;
 	end
 end
 end
