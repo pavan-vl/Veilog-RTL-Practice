fe83f6d3b114e650f828c1053a8773b3eb9ff260
Pavan VL <pavan.vl786@gmail.com>
2024-09-28 23:03:44 +0530

    Add files via upload

---
+++ Status Flag Block/status_flag_blk.v
@@ -0,0 +1,18 @@
+//Designed by Pavan V L
+module status_flag_blk(rdata,master_ready,valid,sin);
+input rdata,master_ready,valid;
+output sin;
+
+wire dff_out;
+
+dff dff_inst(valid,rdata,1'b1,dff_out);
+
+wire nor1_o,nor2_o;
+
+assign nor1_o= ~(rdata | nor_o);
+
+assign nor2_o= ( nor1_o | ( (~master_ready) & dff_out ) );
+
+assign sin= nor1_o;
+
+endmodule
