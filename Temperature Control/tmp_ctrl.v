f3b13dfa1298e10c278afce235d0e7bfa91bb938
Pavan VL <pavan.vl786@gmail.com>
2024-10-13 23:11:43 +0530

    Update tmp_ctrl.v

--- Temperature Control/tmp_ctrl.v
+++ Temperature Control/tmp_ctrl.v
@@ -1,3 +1,28 @@
 //Designed by Pavan V L
-module tmp_ctrl();
+module tmp_ctrl(temp,low_thresh,high_thresh,cool,heat,rst); //Simple Implementation of Temperature Control
+input [16:0]temp,low_thresh,high_thresh;
+input rst;
+output reg cool,heat;
+
+always @ * begin
+if(rst) begin
+cool=0;
+heat=0;
+end
+else begin
+	if( temp >= high_thresh) begin
+		cool=1;
+		heat=0;
+	end
+	else if (temp <= low_thresh) begin
+		cool=0;
+		heat=1;
+	end
+	else if ( temp> low_thresh && temp <high_thresh) begin
+		cool=0;
+		heat=0;
+	end
+end
+end
+
 endmodule
