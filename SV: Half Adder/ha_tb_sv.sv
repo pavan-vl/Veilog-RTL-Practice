0c6048d274c5b8a115ba5404e21c512e0009d3a9
Pavan VL <pavan.vl786@gmail.com>
2024-10-13 22:47:04 +0530

    Add files via upload

---
+++ SV: Half Adder/ha_tb_sv.sv
@@ -0,0 +1,19 @@
+//Designed by Pavan V L
+module ha_tb_sv;
+logic a,b;
+logic s,co;
+
+ha ha_inst(a,b,s,co);
+
+initial begin
+a=0;
+b=0;
+$monitor(" a=%0b ",a," b=%0b " ,b," sum=%0b ",s," carry out=%0b ",co);
+#40 $finish;
+end
+
+always 
+#10 a=~a;
+always
+#20 b=~b;
+endmodule
